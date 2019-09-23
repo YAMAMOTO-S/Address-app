ee1decee570c9032f18abd4c5b3f292e55e4110f
Shunya <yamasyunizig1216@gmail.com>
2019-09-23 21:39:53 +0900

    set firebase

--- src/main.js
+++ src/main.js
@@ -3,8 +3,30 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBYPo4a1KYEnutIfRlMUO2wZGmYoW9AR7o",
+    authDomain: "my-address-pj-969c1.firebaseapp.com",
+    databaseURL: "https://my-address-pj-969c1.firebaseio.com",
+    projectId: "my-address-pj-969c1",
+    storageBucket: "",
+    messagingSenderId: "358965838788",
+    appId: "1:358965838788:web:59409cde1987da03609a91"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+
+
+
 
 new Vue({
   router,
