063100525f939d9a40c8514cd30039800b75d2bc
Shunya <yamasyunizig1216@gmail.com>
2019-09-25 18:19:01 +0900

    編集機能の追加

--- src/store.js
+++ src/store.js
@@ -21,7 +21,8 @@ export default new Vuex.Store({
     toggleSideMenu (state) {
       state.drawer = !state.drawer
     },
-    addAddress (state, address) {
+    addAddress (state, {id, address}) {
+      address.id = id
       state.addresses.push(address)
     }
   },
@@ -44,12 +45,15 @@ export default new Vuex.Store({
       commit('toggleSideMenu')
     },
     addAddress ({ getters, commit }, address) {
-      if (getters.uid) firebase.firestore().collection(`/users/${getters.uid}/addresses`).add(address)
-      commit('addAddress', address)
+      if (getters.uid) { 
+        firebase.firestore().collection(`/users/${getters.uid}/addresses`).add(address).then(doc => { 
+      commit('addAddress', { id: doc.id, address })
+       })
+      }
     },
     fetchAddresses ({getters, commit}) {
       firebase.firestore().collection(`/users/${getters.uid}/addresses`).get().then(snapshot => {
-        snapshot.forEach(doc => commit('addAddress', doc.data()))
+        snapshot.forEach(doc => commit('addAddress', { id: doc.id, address: doc.data()}))
       })
     }
   },
